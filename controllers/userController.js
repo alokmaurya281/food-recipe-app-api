2c971b3a65bdc4036f517230068c27a172f480a9
Alok Maurya <alok.maurya@w3villa.com>
2023-11-01 11:14:15 +0530

    reset pass fixed

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,23 +2,20 @@ const asyncHandler = require("express-async-handler");
 const User = require("../models/userModel");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
-const nodemailer = require('nodemailer');
-const otpGenerator = require('otp-generator');
-
-
+const nodemailer = require("nodemailer");
+const otpGenerator = require("otp-generator");
 
 // Email configuration
 const transporter = nodemailer.createTransport({
-    host: "smtp-relay.brevo.com",
-    port: 587,
-    // secure: true,
-    auth: {
-      // TODO: replace `user` and `pass` values from <https://forwardemail.net>
-      user: "snmaurya10275@gmail.com",
-      pass: "VXUDmHkQYA2Gqjgx",
-    },
-  });
-  
+  host: "smtp-relay.brevo.com",
+  port: 587,
+  // secure: true,
+  auth: {
+    // TODO: replace `user` and `pass` values from <https://forwardemail.net>
+    user: "snmaurya10275@gmail.com",
+    pass: "VXUDmHkQYA2Gqjgx",
+  },
+});
 
 // Regsiter new user
 // route POST /api/v1/user/register
@@ -53,8 +50,13 @@ const registerUser = asyncHandler(async (req, res) => {
 
   if (user) {
     res.status(201).json({
-      data: { _id: user.id, name:user.name, email: user.email, username: user.username },
-      message: 'Registration Successful'
+      data: {
+        _id: user.id,
+        name: user.name,
+        email: user.email,
+        username: user.username,
+      },
+      message: "Registration Successful",
     });
   } else {
     res.status(400);
@@ -76,122 +78,121 @@ const loginUser = asyncHandler(async (req, res) => {
 
   // compare pass with hashed pass
   if (user && (await bcrypt.compare(password, user.password))) {
-    const accessToken = jwt.sign({
-        user:{
-            username: user.username,
-            email: user.email,
-            name: user.name,
-            _id: user.id
-        }
-    }, process.env.JWT_SECRET_KEY,
-    {expiresIn: '60 days'});
+    const accessToken = jwt.sign(
+      {
+        user: {
+          username: user.username,
+          email: user.email,
+          name: user.name,
+          _id: user.id,
+        },
+      },
+      process.env.JWT_SECRET_KEY,
+      { expiresIn: "60 days" }
+    );
     res.status(200).json({ accessToken: accessToken });
-  }
-  else{
+  } else {
     res.status(400);
     throw new Error("Email or password is not vaild !");
   }
 });
 
 // reset password
-// route /api/v1/user/forgotpassword
+// route /api/v1/user/resetpass
 //accesspublic
 
-const resetPass =  asyncHandler(async (req, res)=>{
-    const {email, password} = req.body
-    
-    if(!email && !password){
-        res.status(400);
-        throw new Error("All fields required !");
-    }
-    const hashedPass = await bcrypt.hash(password, 10);
-    const user = await user.findOne({email})
-    if(!user){
-        res.status(400);
-     throw new Error("user not availbale!");
-    }else{
-        await User.findOneAndUpdate(
-            {email},
-            {password: hashedPass}
-        );
-        res.status(200).json({
-            message: "Password Updated SuccessFully"
-        })
-    }
+const resetPass = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email && !password) {
+    res.status(400);
+    throw new Error("All fields required !");
+  }
+  const hashedPass = await bcrypt.hash(password, 10);
+  const user = await User.findOne({ email });
+  // res.status(200).json({
+  //     user
+  // })
+  if (user) {
+    await User.findOneAndUpdate({ email }, { password: hashedPass });
+    res.status(200).json({
+      message: "Password Updated SuccessFully",
+    });
+  }
+  else{
+    res.status(400);
+    throw new Error("User is Invalid !");
+  }
+
+  // else{
+  //
 });
 
 // forgot password
 // route /api/v1/user/forgotpassword
 //accesspublic
-const OTP =  otpGenerator.generate(4, { upperCaseAlphabets: false, specialChars: false, alphabets: false, digits: true,lowerCaseAlphabets: false });
-
-const forgotPass =  asyncHandler(async (req, res)=>{
-    const {email} = req.body;
-    if(!email){
-        res.status(400);
-        throw new Error("All fields required !");
-    }
-    const user = await User.findOne({email})
-    const mailOptions = {
-        from: 'snmaurya10275@gmail.com',
-        to: email, // The recipient's email address
-        subject: 'Your OTP Code to Verify email :: Food Recipe App',
-        text: `Your OTP code is ${OTP}`
-      };
-    
-    if(!user){
-        res.status(400);
-     throw new Error("user not availbale!");
-    }
-    else{
-        await User.findOneAndUpdate(
-            {email},
-            {otp: OTP}
-        );
-        transporter.sendMail(mailOptions, (error, info) => {
-            if (error) {
-              throw new Error(error);
-            }
-            res.status(201).json({
-            data: { otp: OTP },
-            message: 'OTP sent Successfully'
-          });
-        });
-        
-    }
+const OTP = otpGenerator.generate(4, {
+  upperCaseAlphabets: false,
+  specialChars: false,
+  alphabets: false,
+  digits: true,
+  lowerCaseAlphabets: false,
 });
 
+const forgotPass = asyncHandler(async (req, res) => {
+  const { email } = req.body;
+  if (!email) {
+    res.status(400);
+    throw new Error("All fields required !");
+  }
+  const user = await User.findOne({ email });
+  const mailOptions = {
+    from: "snmaurya10275@gmail.com",
+    to: email, // The recipient's email address
+    subject: "Your OTP Code to Verify email :: Food Recipe App",
+    text: `Your OTP code is ${OTP}`,
+  };
 
-const accountVerify = asyncHandler(async (req, res)=>{
-    const {otp, email} = req.body;
-    if (!otp && !email) {
-      res.status(400);
-      throw new Error("All Fields are required!");
-    } 
-    const user = await User.findOne({email});
-    const prevOtp = user.otp;
-    if (user) {
-        if(prevOtp == otp){
-        await User.findOneAndUpdate(
-          {email},
-          {otp: ''},
-        ); 
-        res.status(200).json({
-          
-          message: 'OTP verified Successfully'
-        })
+  if (!user) {
+    res.status(400);
+    throw new Error("user not availbale!");
+  } else {
+    await User.findOneAndUpdate({ email }, { otp: OTP });
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        throw new Error(error);
       }
-      else{
-        res.status(400);
-        throw new Error("Invaild otp!");
-      }
-    }
-    else{
-      res.status(400);
-      throw new Error("user not availbale !");
-    }
-  });
+      res.status(201).json({
+        data: { otp: OTP },
+        message: "OTP sent Successfully",
+      });
+    });
+  }
+});
 
+const accountVerify = asyncHandler(async (req, res) => {
+  const { otp, email } = req.body;
+  if (!otp && !email) {
+    res.status(400);
+    throw new Error("All Fields are required!");
+  }
+  const user = await User.findOne({ email });
+  const prevOtp = user.otp;
+  if (user) {
+    if (prevOtp == otp) {
+      await User.findOneAndUpdate({ email }, { otp: "" });
+      res.status(200).json({
+        message: "OTP verified Successfully",
+      });
+    } else {
+      res.status(400);
+      throw new Error("Invaild otp!");
+    }
+  } else {
+    res.status(400);
+    throw new Error("user not availbale !");
+  }
+});
 
 // get current user
 // route get /api/v1/user/profile
